693289bd2aa7f3a762f0949c17a31e4d81b63852
ferhatergun <eferhat718@gmail.com>
2023-12-27 19:46:06 +0300

    search bar bağlandı ekrana listeleme olayları düzeltildi toast eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import SelectBox from './components/SelectBox'
 import items from './items.json'
 import { useQuery } from 'react-query'
 import { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 
 interface Item {
   data: string[] | undefined
@@ -11,13 +12,14 @@ interface Item {
 
 export const App = () => {
   const [selectItems, setSelectItems] = useState<string[]>([])
-  console.log(selectItems)
+  const [search, setSearch] = useState<string>('')
 
   const fetchData = () => {
     return Promise.resolve(items.data)
   }
 
   useEffect(()=>{
+    //sayfa yenilendiği zaman localstorage'dan verileri çeker
     const items = localStorage.getItem('items')
     if(items){
       setSelectItems(JSON.parse(items))
@@ -32,25 +34,48 @@ export const App = () => {
         <p className="pl-1 text-md">
           Kategoriler
         </p>
-        <input type="text" placeholder="Kategori Ara .."
+        <input type="text" placeholder="Kategori ara.."
         className="border-[1.5px] border-gray-300 text-sm outline-none
-        rounded-md w-full h-8 p-2 mt-2 " />
-        <div className="h-44 overflow-y-scroll content mt-2"> 
-          { !isLoading ?
-            data?.map((item:string,index:number)=>(
+        rounded-md w-full h-8 p-2 mt-2" 
+        onChange={(e)=>setSearch(e.target.value)}
+        />
+        {/* key değeri selectItems de her değişiklik yapıldığında divi render etmek için kullanıldı */}
+        <div className="h-44 overflow-y-scroll content mt-2" key={selectItems.length}> 
+          {
+            //seçili seçenekleri en üstte gösterdik
+            data?.filter((item)=>selectItems.includes(item))
+            .map((item:string,index:number)=>(
               <SelectBox 
               key={index} 
               itemName={item} 
               setSelectItems={setSelectItems} 
               isSelected={selectItems.includes(item)}
               />
-            )):
-            <p>Yükleniyor</p>
+            ))
+          }
+          {
+            //seçili olmayan seçenekler
+            data?.filter((item)=>!selectItems.includes(item))
+            .map((item:string,index:number)=>(
+              //arama kelimesi item içinde varsa ekranda gösterilir
+              item.toLowerCase().includes(search.toLowerCase()) &&
+              <SelectBox 
+              key={index} 
+              itemName={item} 
+              setSelectItems={setSelectItems} 
+              isSelected={selectItems.includes(item)}
+              />
+            ))
           }
         </div>
         <button className="bg-blue-600 text-sm text-white w-full h-8 
         mt-2 rounded-md" 
-        onClick={()=>localStorage.setItem('items',JSON.stringify(selectItems))}>
+        onClick={()=>{
+          
+          localStorage.setItem('items',JSON.stringify(selectItems))
+          toast.success('Seçimleriniz kaydedildi')
+        
+        }}>
           <p>Ara</p>
         </button>
       </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 import './index.css'
 import { QueryClient ,QueryClientProvider } from 'react-query'
+import { Toaster } from 'react-hot-toast'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster/>      
   </QueryClientProvider>,
 )
